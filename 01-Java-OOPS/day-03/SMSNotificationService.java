95145ff245aaa231313b2fbc47b10f72f1e4ba2a
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2026-01-06 00:12:33 +0530

    exercise-2

---
+++ 01-Java-OOPS/day-03/SMSNotificationService.java
@@ -0,0 +1,7 @@
+public class SMSNotificationService implements NotificationService {
+
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
