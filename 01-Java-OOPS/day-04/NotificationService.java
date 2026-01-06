3621be27294dd216cd3efdd6bf41290955ebd3f7
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2026-01-07 00:33:58 +0530

    exercise-1

---
+++ 01-Java-OOPS/day-04/NotificationService.java
@@ -0,0 +1,8 @@
+package com.example.springcore;
+
+// Interface remains same as Day 3
+// Spring works BEST with interfaces
+
+public interface NotificationService {
+    void send(String message);
+}
