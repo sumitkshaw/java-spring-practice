776cfa0930dc671913c009d4f9e9af11bf6cc5b4
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2026-01-20 00:12:49 +0530

    exercise-1

---
+++ 04-spring-data-jpa/day-10/User.java
@@ -0,0 +1,17 @@
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public User() {}
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    // getters & setters
+}
